b4799915fb0d7ceef50a9a91acd021ee59d3e16c
lliodde <gapuzfernandofej@gmail.com>
2025-07-04 16:21:39 -0400

    add Find Doctor

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Import your components
 import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/LandingPage/LandingPage';
-import SignUp from './Components/SignUp/SignUp'; // Import Sign Up
-import Login from './Components/Login/Login';       // Import Login
+import SignUp from './Components/SignUp/SignUp';
+import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch'; // 1. Import the new component
 
 function App() {
   return (
@@ -14,11 +15,12 @@ function App() {
       <>
         <Navbar />
         <Routes>
-          {/* Add all the routes for your pages */}
+          {/* All your application routes */}
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/instant-consultation" element={<InstantConsultation />} />
+          <Route path="/find-doctor" element={<FindDoctorSearch />} /> {/* 2. Add the new route */}
         </Routes>
       </>
     </BrowserRouter>
